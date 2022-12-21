6bde18f9602d7c13c77cfcfbd61190d2e2d12b59
2takecarl <carlsterner@outlook.com>
2022-12-21 23:12:19 +0100

    some message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import {DoubleHeader} from './components/Header'
+import { HeroBullets } from './components/Hero';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      {/*<Welcome />
+      <ColorSchemeToggle />*/}
+      <DoubleHeader />
+      <HeroBullets />
+
     </>
   );
 }
